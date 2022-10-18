e694dbbd4ef0d4088218959199f2106b5801556d
bruno.roma <brunoo.roma@gmail.com>
2022-10-17 21:33:43 -0300

    desafio clase 8 - form

--- Clase8/app.js
+++ Clase8/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const routerProductos = require('./routes/productos.js');
 
 const PORT = 8080;
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use('/api', routerProductos)
+
 const server = app.listen(process.env.PORT || PORT, () =>
   console.log(`Server listening on PORT ${PORT}`)
 );
+
 server.on("error", (err) => console.log(`Error: ${err}`));
-
-const routerProductos = require('./routes/productos.js');
-
-app.use('/api', routerProductos)
